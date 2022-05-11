cc6e1e77a5a31298a8f04c521da586c1a44ccc44
anton_dirani <antondirani@outlook.com>
2022-05-12 01:43:17 +0300

    test

--- lib/Screens/trip_planner.dart
+++ lib/Screens/trip_planner.dart
@@ -10,9 +10,13 @@ class TripPlanner extends StatelessWidget {
     return Scaffold(
       backgroundColor: mainColor,
       appBar: NewGradientAppBar(
+          centerTitle: true,
           //backgroundColor: Color(0xFF00D3A0),
           elevation: 0,
-          title: Text('Trip planner'),
+          title: Text(
+            'Trip Planner',
+            style: GoogleFonts.montserrat(fontWeight: FontWeight.w500),
+          ),
           gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
@@ -27,6 +31,21 @@ class TripPlanner extends StatelessWidget {
       body: Column(
         children: [
           LocationSelector(),
+          SizedBox(
+            height: 30,
+            width: double.infinity,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+              width: double.infinity,
+            ),
+          )
         ],
       ),
     );
@@ -39,8 +58,8 @@ class LocationSelector extends StatelessWidget {
     return Column(
       children: [
         Container(
-          padding: EdgeInsets.all(15.0),
           color: Colors.white,
+          padding: EdgeInsets.all(12.0),
           child: Row(
             children: [
               Expanded(
@@ -67,19 +86,27 @@ class LocationSelector extends StatelessWidget {
                             Text(
                               'From',
                               style: GoogleFonts.montserrat(
-                                  fontSize: 16,
+                                  fontSize: 14,
                                   fontWeight: FontWeight.w400,
                                   color: Colors.grey),
                             ),
-                            Text(
-                              'Choose your location',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.grey[850]),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                'Choose your location',
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.grey[850]),
+                              ),
                             ),
                           ],
                         ),
+                        Spacer(),
+                        Icon(
+                          Icons.my_location,
+                          color: Colors.grey[850],
+                        )
                       ],
                     ),
                     const Divider(
@@ -94,9 +121,9 @@ class LocationSelector extends StatelessWidget {
                             height: 15,
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
-                              color: Colors.blue.withOpacity(.3),
-                              border:
-                                  Border.all(color: Colors.orange, width: 3.0),
+                              color: Colors.deepOrangeAccent.withOpacity(.3),
+                              border: Border.all(
+                                  color: Colors.deepOrangeAccent, width: 3.0),
                             )),
                         SizedBox(
                           width: 15,
@@ -105,20 +132,30 @@ class LocationSelector extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              'From',
+                              'To',
                               style: GoogleFonts.montserrat(
-                                  fontSize: 16,
+                                  fontSize: 14,
                                   fontWeight: FontWeight.w400,
                                   color: Colors.grey),
                             ),
-                            Text(
-                              'Choose your destination',
-                              style: GoogleFonts.montserrat(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.grey[850]),
+                            GestureDetector(
+                              onTap: () {
+                                print('dest');
+                              },
+                              child: Text(
+                                'Choose your destination',
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.grey[850]),
+                              ),
                             ),
                           ],
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.swap_vert,
+                          color: Colors.grey[850],
                         )
                       ],
                     )
@@ -127,9 +164,14 @@ class LocationSelector extends StatelessWidget {
           ),
         ),
         MaterialButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          )),
           minWidth: double.infinity,
           color: Colors.white,
-          height: MediaQuery.of(context).size.height / 12,
+          height: MediaQuery.of(context).size.height / 14,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -137,9 +179,15 @@ class LocationSelector extends StatelessWidget {
                 Icons.search,
                 color: mainColor,
               ),
+              SizedBox(
+                width: 3,
+              ),
               Text(
-                'Search',
-                style: TextStyle(fontSize: 22, color: mainColor),
+                'Search Now',
+                style: GoogleFonts.montserrat(
+                    fontSize: 21,
+                    color: mainColor,
+                    fontWeight: FontWeight.w500),
               ),
             ],
           ),
