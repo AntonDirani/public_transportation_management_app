2af4a6625aafcbaf41e1c4edf8a4846d0c8feda8
anton_dirani <antondirani@outlook.com>
2022-05-12 03:31:05 +0300

    Sign up updates

--- lib/Screens/sign_up_page.dart
+++ lib/Screens/sign_up_page.dart
@@ -2,14 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class sign_up_page extends StatefulWidget {
   const sign_up_page({Key? key}) : super(key: key);
 
   @override
   State<sign_up_page> createState() => _sign_up_pageState();
 }
-
 
 class _sign_up_pageState extends State<sign_up_page> {
   @override
@@ -22,6 +20,7 @@ class _sign_up_pageState extends State<sign_up_page> {
     );
   }
 }
+
 class signUp extends StatefulWidget {
   const signUp({Key? key}) : super(key: key);
   static String id = 'signUp';
@@ -31,150 +30,176 @@ class signUp extends StatefulWidget {
 
 class _signUpState extends State<signUp> {
   @override
-
   Widget build(BuildContext context) {
-    var MediaHeight= MediaQuery.of(context).size.height ;
-    var MediaWidth= MediaQuery.of(context).size.width;
-    return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-        ),
+    var MediaHeight = MediaQuery.of(context).size.height;
+    var MediaWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
         child: Stack(
           children: [
             Container(
-              height:MediaHeight / 3,
+              height: MediaHeight / 3,
               width: MediaWidth / 1,
               decoration: BoxDecoration(
-                  image:
-                  DecorationImage(image: AssetImage('assets/Road.png'))),
+                  image: DecorationImage(image: AssetImage('assets/Road.png'))),
             ),
-          Padding(
-            padding: EdgeInsets.only( left: MediaWidth * 0.07, right: MediaWidth * 0.07),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: MediaHeight * 0.3,),
-                Text('Sign Up' , style: GoogleFonts.montserrat( color:   Colors.black , fontSize: 35 , fontWeight: FontWeight.w600),),
-                SizedBox(height: MediaHeight * 0.03,),
-                TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide:
-                        BorderSide(width: 2, color: Color(0xff33C58E))),
-                    labelText: 'Name',
-                    labelStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(
-                      Icons.account_circle,
-                      color: Color(0xff33C58E),
-                    ),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: MediaWidth * 0.07,
+                  right: MediaWidth * 0.07,
+                  bottom: MediaWidth * 0.07),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaHeight * 0.3,
                   ),
-                  keyboardType: TextInputType.name,
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaHeight * 0.03,),
-                TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide:
-                        BorderSide(width: 2, color: Color(0xff33C58E))),
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Color(0xff33C58E),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaHeight * 0.03,),
-                TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide:
-                        BorderSide(width: 2, color: Color(0xff33C58E))),
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Color(0xff33C58E),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaHeight * 0.03,),
-                TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide:
-                        BorderSide(width: 2, color: Color(0xff33C58E))),
-                    labelText: 'Confrim Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Color(0xff33C58E),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaHeight * 0.03,),
-                TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide:
-                        BorderSide(width: 2, color: Color(0xff33C58E))),
-                    labelText: 'National Number',
-                    labelStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(
-                      Icons.numbers,
-                      color: Color(0xff33C58E),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(height: MediaHeight * 0.03,),
-                MaterialButton(
-                  minWidth: double.infinity,
-                  color: Color(0xff33C58E),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  height: MediaQuery.of(context).size.height / 12,
-                  child: Text(
+                  Text(
                     'Sign Up',
-                    style: TextStyle(fontSize: 22, color: Colors.white),
+                    style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600),
                   ),
-                  onPressed: () {
-                    //Navigator.pushNamed(context, welcome.id);
-                  },
-                ),
-              ],
-            ),
-          )
-
+                  Text(
+                    'Enter your information.',
+                    style: GoogleFonts.montserrat(
+                        color: Colors.grey[700],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.02,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xff33C58E))),
+                      labelText: 'Name',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.account_circle,
+                        color: Color(0xff33C58E),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xff33C58E))),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Color(0xff33C58E),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xff33C58E))),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Color(0xff33C58E),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xff33C58E))),
+                      labelText: 'Confrim Password',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Color(0xff33C58E),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.01,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xff33C58E))),
+                      labelText: 'National Number',
+                      labelStyle: TextStyle(color: Colors.black),
+                      prefixIcon: Icon(
+                        Icons.numbers,
+                        color: Color(0xff33C58E),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: MediaHeight * 0.03,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    color: Color(0xff33C58E),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    height: MediaQuery.of(context).size.height / 12,
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 22, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      //Navigator.pushNamed(context, welcome.id);
+                    },
+                  ),
+                ],
+              ),
+            )
           ],
-
         ),
-
       ),
     );
   }
